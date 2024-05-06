b41c67ea24738f0d707acab6db4e4da3a3ee2771
NicholasBuiatti <nic.buiatti@gmail.com>
2024-05-06 16:33:53 +0200

    m-2 stampato in pagina tutti i valori delle chiavi degli oggetti sotto forma di stringa

--- js/script.js
+++ js/script.js
@@ -34,11 +34,31 @@ const team = [ teamMate1, teamMate2, teamMate3, teamMate4, teamMate5, teamMate6]
 
 console.log('questo è il tuo team: ', team);
 
+let h1Name = document.querySelector('#nameTeam');
+
 //ciclo che itera per ogni oggetto nell'array
 for (let i = 0; i < team.length; i++) {
     const mate = team[i];
     console.log(mate);
-    const nameMate = mate.nome;
-    const roleMate = mate.ruolo;
-    console.log('nome:', nameMate,'ruolo:', roleMate);
+
+    for (const key in mate) {
+        console.log(key, mate[key])
+        h1Name.innerHTML += `<h1> ${key}: ${mate[key]} </h1>`;
+    }
+
+    
 }
+
+
+/*
+let h1Role = document.querySelector('#roleTeam');
+let imgTeam = document.querySelector('#imgProfile')
+const nameMate = mate.nome;
+    h1Name.innerHTML += `<h1> nome: ${nameMate} </h1>`;
+    const roleMate = mate.ruolo;
+    h1Role.innerHTML += `<h1> ruolo: ${roleMate} </h1>`;
+    console.log('nome:', nameMate,'ruolo:', roleMate);
+    const imgProfile = mate.immagine;
+    console.log('foto:', imgProfile);
+    imgTeam.innerHTML += `<img src=./img/${imgProfile}>`
+    */
