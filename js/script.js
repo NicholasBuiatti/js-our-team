7ca2926569307018d99e5fa40e32c9c4c9e6bde3
NicholasBuiatti <nic.buiatti@gmail.com>
2024-05-06 15:22:15 +0200

    m-1 creazione array di oggetti

---
+++ js/script.js
@@ -0,0 +1,40 @@
+
+const teamMate1 = {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    immagine: 'wayne-barnett-founder-ceo.jpg'
+}
+
+const teamMate2 = {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    immagine: 'angela-caroll-chief-editor.jpg'
+}
+
+const teamMate3 = {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    immagine: 'walter-gordon-office-manager.jpg'
+}
+
+const teamMate4 = {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    immagine: 'angela-lopez-social-media-manager.jpg'
+}
+
+const teamMate5 = {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    immagine: 'scott-estrada-developer.jpg'
+}
+
+const teamMate6 = {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    immagine: 'barbara-ramos-graphic-designer.jpg'
+}
+
+const team = [ teamMate1, teamMate2, teamMate3, teamMate4, teamMate5, teamMate6];
+
+console.log('questo è il tuo team: ', team);
